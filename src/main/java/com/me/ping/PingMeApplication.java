58b1e4b07bf0ffb26ba57e8c566a46f5e1e34295
rsillador <rhaynel.samar.sillador@gmail.com>
2018-04-14 23:43:00 +0800

    initial commit

---
+++ src/main/java/com/me/ping/PingMeApplication.java
@@ -0,0 +1,12 @@
+package com.me.ping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingMeApplication.class, args);
+	}
+}
